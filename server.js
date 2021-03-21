5c75f18ef9bc064c49b584697b77ccc42fca604e
Steven Elliott <steven.elliott@live.com>
2021-03-20 21:22:34 -0400

    adding CLI

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var inquirer = require('inquirer');
 var connection = require('./connection');
 
+const options = [
+    "View Departments",
+    "View Roles",
+    "View Employees",
+    "Update Employee",
+    "exit"
+];
+
+const empOptions = [
+    
+]
